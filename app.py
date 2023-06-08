7ea3c44bdebd44d3a87d87a8d2036f93417898b9
Darren Conyngham <dconyngham92@gmail.com>
2023-06-08 20:38:35 +0200

    add support for google sheet

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas_profiling
+from ydata_profiling import ProfileReport
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 
@@ -7,10 +7,38 @@ from streamlit_pandas_profiling import st_profile_report
 st.title("Automatic Exploratory Data Analysis")
 st.subheader("Upload your dataset and get a full analysis on it in seconds!")
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+uploaded_file = None
+url = ""
 
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, index_col=None)
+option = st.selectbox(
+    'What type of dataset do you have?',
+    ('CSV', 'Google Sheet'))
+
+if option == 'CSV':
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+elif option == 'Google Sheet':
+    url = st.text_input("Paste a link to a public Google Sheet here", value="")
+
+# while uploaded_file is None or url == "":
+#     pass
+
+# while uploaded_file is None or url == "":
+#     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+#     st.write("OR paste a link to a Google Sheet below")
+
+#     url = st.text_input("Paste a link to a public Google Sheet here")
+
+    # if uploaded_file is None or uploaded_file == "":
+    #     st.warning("Upload a CSV file or paste a link to a Google Sheet above")
+
+
+if uploaded_file is not None or url != "":
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file, index_col=None)
+    else:
+        df = pd.read_csv(url)
 
     st.subheader("Basic summary statistics of the dataset")
     st.write(
@@ -21,7 +49,9 @@ if uploaded_file is not None:
     st.dataframe(df.head())
 
     st.subheader("Auto EDA report")
-    profile = df.profile_report(minimal=True)
+    # profile = df.profile_report(minimal=True)
+    profile = ProfileReport(df, minimal=True)
+
     profile.to_file('auto_eda_report.html')
 
     with open('auto_eda_report.html', 'r') as f:
