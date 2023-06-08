d59760b47621988ca21735c6c6bfa80323c6cb1a
Darren Conyngham <dconyngham92@gmail.com>
2023-06-08 21:30:50 +0200

    add more info to report

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas_profiling
+from ydata_profiling import ProfileReport
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 
@@ -12,16 +12,25 @@ uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, index_col=None)
 
+    st.divider()
+
     st.subheader("Basic summary statistics of the dataset")
     st.write(
         f"- There are {df.shape[0]:,} rows and {df.shape[1]:,} columns in this dataset.")
     st.write(f"- The dataset has {df.isna().sum().sum()} missing values.")
 
+    st.divider()
+
     st.subheader("First five rows of the dataset")
     st.dataframe(df.head())
 
+    st.subheader("Descriptive statistics of the dataset")
+    st.dataframe(df.describe(include='all'))
+
+    st.divider()
+
     st.subheader("Auto EDA report")
-    profile = df.profile_report(minimal=True)
+    profile = ProfileReport(df, minimal=True)
     profile.to_file('auto_eda_report.html')
 
     with open('auto_eda_report.html', 'r') as f:
